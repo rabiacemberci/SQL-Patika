82607c401ef2edd78e254f1e4dd8d6f394acfe08
Rabia <93681938+rabiacemberci@users.noreply.github.com>
2022-10-29 13:20:11 +0300

    Patika

--- Homework6.sql
+++ Homework6.sql
@@ -1,21 +1,25 @@
 1.Question:
+--Film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
 SELECT ROUND(AVG(rental_rate),3) FROM film;  
 
 ---- 2.980
 
 2.Question:
+--Film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
 SELECT COUNT(title) FROM film
 WHERE title LIKE 'C%';
 
 ---- 92
 
 3.Question:
+--Film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
 SELECT MAX(length) FROM film
 WHERE rental_rate = 0.99;
 
 ---- 184
 
 4.Question:
+--Film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 SELECT 	COUNT(DISTINCT replacement_cost) FROM film 
 WHERE length > 150;
 
