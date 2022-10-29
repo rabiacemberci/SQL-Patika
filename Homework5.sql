6b54309045b4941f1be10478540ad29eeb564b42
Rabia <93681938+rabiacemberci@users.noreply.github.com>
2022-10-29 13:17:30 +0300

    Patika

--- Homework5.sql
+++ Homework5.sql
@@ -1,4 +1,6 @@
 1.Question:
+--Film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten 
+--en uzun (length) 5 filmi sıralayınız.
 SELECT title FROM film
 WHERE title
 LIKE '%n'
@@ -6,6 +8,8 @@ ORDER BY length DESC
 LIMIT 5
 
 2.Question:
+--Film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) 
+--ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
 SELECT * FROM film
 WHERE title
 LIKE '%n'
@@ -14,6 +18,8 @@ OFFSET 5
 LIMIT 5
 
 3.Question:
+--Customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 
+--olmak koşuluyla ilk 4 veriyi sıralayınız.
 SELECT * FROM customer
 WHERE store_id = '1'
 ORDER BY last_name DESC
